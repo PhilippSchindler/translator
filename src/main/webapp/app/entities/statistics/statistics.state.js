27284ad722b587a3e7de7036910e1a6d851caf00
Clemens Fickl <e1129315@student.tuwien.ac.at>
2016-12-19 23:03:47 +0100

    [#34] added google charts, frontend impl and backend mock for not translated texts
    Signed-off-by: Clemens Fickl <e1129315@student.tuwien.ac.at>

---
+++ src/main/webapp/app/entities/statistics/statistics.state.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('translatorApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider
+            .state('statistics', {
+                parent: 'entity',
+                url: '/project/{projectId}/statistics',
+                data: {
+                    authorities: ['ROLE_CUSTOMER', 'ROLE_TRANSLATOR'],
+                    pageTitle: 'translatorApp.statistics.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/entities/statistics/statistics.html',
+                        controller: 'StatisticsController',
+                        controllerAs: 'vm'
+                    }
+                },
+                resolve: {
+                    mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('home');
+                        $translatePartialLoader.addPart('statistics');
+                        return $translate.refresh();
+                    }],
+                    notTranslatedTexts: ['$stateParams', 'Statistics', function ($stateParams, Statistics) {
+                        return Statistics.getNotTranslatedTexts({projectId: $stateParams.projectId}).$promise;
+                    }]
+                }
+            });
+    }
+})();
