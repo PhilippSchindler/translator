449824ef6cf9f6552ae80b9ccc84b0953f693ffc
Philipp Schindler <philipp.schindler@live.at>
2017-01-31 14:23:08 +0100

    [#43] notifications for missing translations implemented, interval set at 1 minute

--- src/main/java/at/ac/tuwien/translator/service/ReleaseService.java
+++ src/main/java/at/ac/tuwien/translator/service/ReleaseService.java
@@ -4,13 +4,18 @@ import at.ac.tuwien.translator.domain.*;
 import at.ac.tuwien.translator.dto.SelectedVersion;
 import at.ac.tuwien.translator.dto.SelectedVersions;
 import at.ac.tuwien.translator.repository.*;
+import at.ac.tuwien.translator.security.AuthoritiesConstants;
 import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Hibernate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZonedDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -23,8 +28,20 @@ public class ReleaseService {
     private static final String MESSAGE_RELEASE_FINISHED = "Hallo %s,\n\ndie Übersetzungsarbeiten für den Release '%s' wurden abgeschlossen.\n" +
         "Damit ist dieser Release nun fertig.\n\nMit freundlichen Grüßen,\ndas Translator-Team";
 
+
+    private static final String SUBJECT_TRANSLATIONS_MISSING = "Fehlende Übersetzungen im Projekt '%s'";
+    private static final String MESSAGE_TRANSLATIONS_MISSING =
+        "Hallo %s %s,\n\n" +
+        "du erhälst diese Benachrichtung, da du am Projekt '%s' als Übersetzer beteiligt bist.\n" +
+        "Am %s ist die Deadline für das Release '%s' und es sind noch Übersetzungen ausständig.\n\n" +
+        "Mit freundlichen Grüßen,\n" +
+        "das Translator-Team";
+
     @Autowired
     private ReleaseRepository releaseRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
 
     @Autowired
     private DefinitionRepository definitionRepository;
@@ -138,18 +155,84 @@ public class ReleaseService {
     }
 
 
-    @Scheduled(cron="*/5 * * * * ?")
-    public void sendNotificationForMissingTranslations()
+    @Scheduled(fixedRate = 1000 * 60) // in ms => 1min derzeit
+    public void checkAndSendNotificationForMissingTranslations()
     {
+        System.out.println();
+        System.out.println();
+        System.out.println("============================================================");
+        System.out.println();
+        System.out.println("Running release notification task for missing translations at " + new Date());
+        System.out.println();
         List<Release> releases = releaseRepository.findAll();
-        for (Release r : releases) {
-            if (!r.isNotificationSent() /* and TODO */) {
-                System.out.println("Method executed at every 5 seconds. Current time is :: " + new Date());
+        System.out.println();
 
-                r.setNotificationSent(true);
-                releaseRepository.save(r);
+        for (Release r : releases) {
+
+            if (r.getState() == ReleaseState.FINISHED) {
+                System.out.println("Notification für Release '" + r.getName() + "' nicht notwendig; ReleaseState == FINISHED");
+                continue;
             }
+
+            if (r.isNotificationSent()) {
+                System.out.println("Notification für Release '" + r.getName() + "' nicht notwendig; Notification bereits versendet");
+                continue;
+            }
+
+            LocalDate in5Days = LocalDate.now().plusDays(5);
+            if (r.getDeadline().isAfter(in5Days)) {
+                System.out.println("Notification für Release '" + r.getName() + "' nicht notwendig; Deadline nicht innerhalb von 5 Tagen");
+                continue;
+            }
+
+            // deadline coming close, check if all translations have been made
+            // and send email if not
+            // ReleaseState.FINISHED is automatic set when translations get added
+
+//            int numNonEnglishLanguages = r.getProject().getLanguages().size();
+//            boolean sendNotification = false;
+//            for (Definition d : r.getDefinitions()) {
+//                if (d.getTranslations().size() < numNonEnglishLanguages)
+//                {
+//                    sendNotification = true;
+//                    break;
+//                }
+//            }
+
+            System.out.println("Notification für Release '" + r.getName() + "' wird versendet");
+            sendNotificationEmails(r);
+
+            r.setNotificationSent(true);
+            releaseRepository.save(r);
         }
 
+
+        System.out.println("============================================================");
+        System.out.println();
+        System.out.println();
+    }
+
+    private void sendNotificationEmails(Release r) {
+
+        Project p = projectRepository.findOneWithEagerRelationships(r.getProject().getId());
+        Set<User> users = p.getUsers();
+
+        Hibernate.initialize(users);
+        for (User u : users) {
+            for (Authority a : u.getAuthorities()) {
+                if (a.getName().equals(AuthoritiesConstants.TRANSLATOR)) {
+                    // this user is a translator for this project, send the email
+                    notificationService.sendEmail(u.getEmail(),
+                        String.format(SUBJECT_TRANSLATIONS_MISSING, r.getProject().getName()),
+                        String.format(MESSAGE_TRANSLATIONS_MISSING,
+                            u.getFirstName(), u.getLastName(),
+                            r.getProject().getName(),
+                            r.getDeadline().toString(),
+                            r.getName())
+                        );
+                    break;
+                }
+            }
+        }
     }
 }
